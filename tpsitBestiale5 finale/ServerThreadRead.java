ef0b407efa77b8457fc416c074620b48d5af6e4b
XOShu4 <94739656+XOShu4@users.noreply.github.com>
2021-12-08 14:57:28 +0100

    Add files via upload

---
+++ tpsitBestiale5 finale/ServerThreadRead.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 22 Giugno anno 0, 2021, SafJNest and/or its affiliates. All rights reserved.
+ * SAFJNEST PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+import java.io.PrintWriter;
+/**
+ * @author XOShu4
+ * avviata da ServerThread si occupa di leggere i messaggi del rispettivo Client dentro MsgBox
+ */
+public class ServerThreadRead extends Thread {
+    /**
+     * PrintWriter passato dal ServerThread
+     */
+    PrintWriter out;
+    /**
+     * oggetto MsgBox passato da ServerThread
+     */
+    MsgBox msgBox;
+    /**
+     * UserName del Client condiviso da ServerThread
+     */
+    String UserName;
+
+    public ServerThreadRead(PrintWriter out, MsgBox msgBox, String UserName) {
+        this.out = out;
+        this.msgBox = msgBox;
+        this.UserName = UserName;
+    }
+
+    public void run() {
+        synchronized (msgBox) {                                                          //metodo sincronizzato
+            while (true) {  
+                try {
+                    out.println(":MsgToDec:/" + msgBox.readBox(UserName));               //chiede ogni volta che puo di leggere la msgBox e il return lo manda al ClientReader 
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
